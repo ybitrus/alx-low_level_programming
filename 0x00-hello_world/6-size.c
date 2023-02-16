71b712e4cf77a4b1e08441bc1fa884612065ebc2
ybitrus <yusufbitrus@gmail.com>
2023-02-16 11:39:56 -0800

    last error fix 'just mistake an and a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,16 +11,15 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
 	char charType;
 	long int long_intType;
 	long long long_longType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(long_intType));
-	printf("Size of long long int %zu byte(s)\n", sizeof(long_longType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_intType));
+	printf("Size of a long long int %zu byte(s)\n", sizeof(long_longType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 		return (0);
 }
