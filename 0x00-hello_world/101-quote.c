3ac5080283e9cf70ac55969a0ba5a366b7e87c7f
ybitrus <yusufbitrus@gmail.com>
2023-02-17 00:17:36 +0100

    tasks 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
- * Return; Always 1 (success)
+ * Return: Always 1 (success)
  */
 
 int main(void)
 
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 
 	return (1);
 }
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,6 @@
+#include <unistd.h> 
+int main() 
+{ 
+  write(1, "Hello World\n", 12); 
+  return 0; 
+} 
