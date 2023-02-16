0d60a5dc42202829e03fc970dc4572a1d7ff7519
ybitrus <yusufbitrus@gmail.com>
2023-02-16 23:47:24 +0100

    tasks 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return; Always 1 (success)
+ */
+
+int main(void)
+
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
