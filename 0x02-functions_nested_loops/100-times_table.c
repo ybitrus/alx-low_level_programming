0da0c6ce38a557458ab6fa1cf15092793c12a5c3
ybitrus <yusufbitrus@gmail.com>
2023-02-22 15:56:03 +0100

    tasks 2 advance

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - Multiplication table
+ * @n: variable 
+ */
+
+void print_times_table(int n)
+{
+	int x, y, result;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				result = x * y;
+
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10)) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
