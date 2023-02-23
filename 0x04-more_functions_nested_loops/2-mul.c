8a6edc8cc4767afd2fbdb45b5bd0a2564c4621df
ybitrus <yusufbitrus@gmail.com>
2023-02-23 18:34:24 +0100

    tasks 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function for multiplication
+ * @a: First assign variable
+ * @b: Second assign variable
+ *
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
