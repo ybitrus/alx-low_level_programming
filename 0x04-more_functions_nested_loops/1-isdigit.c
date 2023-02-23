9cb3ce7be97a821715f3eb2fbccdaf469393a0be
ybitrus <yusufbitrus@gmail.com>
2023-02-23 17:58:55 +0100

    tasks 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - To check for digit
+ * @c: variable number to be check
+ *
+ * Return: 1 if character 0 anything else
+ */
+
+int _isdigit(int c)
+{
+return (c > 40 && c < 50) ? 1 : 0;
+/*	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	*/
+}
