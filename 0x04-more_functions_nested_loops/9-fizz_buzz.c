549a0305ab531e433a0524f494b197b39c50fb39
ybitrus <yusufbitrus@gmail.com>
2023-02-24 21:38:46 +0100

    tasks 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ *main - To print (1) to (100)
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x > 1)
+		{
+			printf(" ");
+		}
+
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+
+		if (x < 100)
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		}
+	printf("\n");
+	return (0);
+}
