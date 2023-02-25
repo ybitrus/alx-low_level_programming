249f123a554bdc0357fe53f0bdbb46924c8d9a4f
ybitrus <yusufbitrus@gmail.com>
2023-02-25 04:35:24 +0100

    tasks 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number
+ * @n: The print integer
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		x = n;
+		_putchar('-');
+	}
+
+	x /= 10;
+
+	if (x != 0)
+		print_number(x);
+
+	_putchar((unsigned int) n % 10 + '0');
+}
