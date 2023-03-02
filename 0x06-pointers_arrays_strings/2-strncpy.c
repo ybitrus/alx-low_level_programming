e3278d7647269bd435b53cdae6220c86262ea982
ybitrus <yusufbitrus@gmail.com>
2023-03-02 20:23:16 +0100

    tasks 2 hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,20 +11,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int x;
-	
+	int x;
+
 	x = 0;
 	while (x < n && src[x] != '\0')
-        {
-                dest[x] = src[x];
-                x++;
-        }
-	
+	{
+		dest[x] = src[x];
+		x++;
+	}
+
 	while (x < n)
 	{
 		dest[x] = '\0';
 		x++;
 	}
-    
-        return (dest);
+
+	return (dest);
 }
