2ad28f20894b91d7b30da24d1f638ed54c83aa38
ybitrus <yusufbitrus@gmail.com>
2023-03-15 15:36:44 +0100

    tasks 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - To print name of a programe
+ * @argc: numbers enter in the command prompt or terminal
+ * @argv: It hold command enter in the terminal
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 0, y = 0;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
