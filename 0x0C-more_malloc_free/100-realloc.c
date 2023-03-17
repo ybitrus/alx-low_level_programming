a2bc458c2995d3ad99afa6bbf21b226214eab699
ybitrus <yusufbitrus@gmail.com>
2023-03-17 13:37:59 +0100

    tasks 101

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -51,10 +51,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size < old_size)
 	{
 
-	for (i = 0; i < new_size; i++)
-	{
+		for (i = 0; i < new_size; i++)
 		ptr1[i] = old_ptr[i];
-	}
+
 	}
 
 
@@ -67,7 +66,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 
 	}
+
 	free(ptr);
+
 	return (ptr1);
 
 }
